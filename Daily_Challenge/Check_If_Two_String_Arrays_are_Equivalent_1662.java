07215c972fad81aec8dcac79e239c99166e402c3
Madhu Kandukuri <70106840+Madhu683@users.noreply.github.com>
2022-10-25 10:42:44 +0530

    StringBuilder+Math.max()

--- Daily_Challenge/Check_If_Two_String_Arrays_are_Equivalent_1662.java
+++ Daily_Challenge/Check_If_Two_String_Arrays_are_Equivalent_1662.java
@@ -1,3 +1,4 @@
+//Approach -1
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         String str1 ="";
@@ -14,3 +15,23 @@ class Solution {
 
 //Time complexity:O(n);
 //Space complexity:O(1);
+
+
+//Approach 2
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+        
+        for(int i=0;i<Math.max(word1.length,word2.length);i++)
+        {
+            if(i<word1.length) str1.append(word1[i]);
+            if(i<word2.length) str2.append(word2[i]);
+            
+        }
+        return (str1.toString()).equals(str2.toString());
+    }
+}
+
+//Time complexity:O(max(m,n))
+//Space complexity:O(1)
